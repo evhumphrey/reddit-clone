1d28bb353ce41f98c8bedf16bb7a8232eccad448
Elliot Humphrey <evhumphrey@users.noreply.github.com>
2017-04-24 14:00:12 -0700

    Refactor SubsController (require user logged in for certain actions)

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,29 +1,22 @@
 class SubsController < ApplicationController
 
+  before_action :require_login, except: [:index, :show]
   before_action :require_mod, only: [:edit, :update, :destroy]
 
   def new
-    # any creation actions must verify that user is logged in
-    if logged_in?
-      @sub = Sub.new
-      render :new
-    else
-      redirect_to new_session_url
-    end
+    @sub = Sub.new
+    render :new
   end
 
   def create
-    if logged_in?
-      @sub = Sub.new(sub_params)
-      @sub.moderator = current_user
-      if @sub.save
-        redirect_to sub_url(@sub)
-      else
-        flash.now[:errors] = @sub.errors.full_messages
-        render :new
-      end
+    @sub = Sub.new(sub_params)
+    @sub.moderator = current_user
+
+    if @sub.save
+      redirect_to sub_url(@sub)
     else
-      redirect_to new_session_url
+      flash.now[:errors] = @sub.errors.full_messages
+      render :new
     end
   end
 
@@ -38,48 +31,39 @@ class SubsController < ApplicationController
     if @sub
       render :show
     else
+      # TODO :404???
       flash[:errors] = [" 🕵️ Can't find sub you're looking for 🕵️‍♀️ "]
       redirect_to subs_url
     end
   end
 
   def edit
-    if logged_in?
-      @sub = Sub.find_by(id: params[:id])
-      if @sub.nil?
-        flash[:errors] = [" 🕵️ Can't find sub you're looking for 🕵️‍♀️ "]
-        redirect_to subs_url
-        return
-      end
-    else
-      redirect_to new_session_url
+    @sub = Sub.find_by(id: params[:id])
+
+    if @sub.nil?
+      flash[:errors] = [" 🕵️ Can't find sub you're looking for 🕵️‍♀️ "]
+      redirect_to subs_url
+      return
     end
   end
 
   def update
-    if logged_in?
-      @sub = Sub.find_by(id: params[:id])
 
-      if @sub.update_attributes(sub_params)
-        @sub.update(sub_params)
-        redirect_to sub_url(@sub)
-      else
-        flash.now[:errors] = @sub.errors.full_messages
-        render :edit
-      end
+    @sub = Sub.find_by(id: params[:id])
+
+    if @sub.update_attributes(sub_params)
+      @sub.update(sub_params)
+      redirect_to sub_url(@sub)
     else
-      redirect_to new_session_url
+      flash.now[:errors] = @sub.errors.full_messages
+      render :edit
     end
   end
 
   def destroy
-    if logged_in?
-      @sub = Sub.find_by(id: params[:id])
-      @sub.destroy
-      redirect_to subs_url
-    else
-      redirect_to new_session_url
-    end
+    @sub = Sub.find_by(id: params[:id])
+    @sub.destroy
+    redirect_to subs_url
   end
 
   private
@@ -94,6 +78,12 @@ class SubsController < ApplicationController
       flash.now[:errors] = [" 🚫 Must be moderator 🚫 "]
       render :show
       return
+    end
+  end
+
+  def require_login
+    unless logged_in?
+      redirect_to new_session_url
     end
   end
 end
